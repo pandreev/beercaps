ab7f855fb5502f59f1e29f866822a289d0cf1270
petar.andreev <petar.andreev@ysura.com>
2025-06-20 15:11:46 +0200

    a

--- europe/ukraine.js
+++ europe/ukraine.js
@@ -1,3 +1,4 @@
 const beerUkraine = [
     {name: 'Lvivske </br> Svitle', brewery: 'Lvivske', country: 'Ukraine', continent: 'Europe', image: 'img/Ukraine/1.png'},
+    {name: 'Avtorske </br> Andriy Matsola', brewery: 'Persha', country: 'Ukraine', continent: 'Europe', image: 'img/Ukraine/Persha/Avtorske.png'},
 ];
