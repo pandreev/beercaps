183f8f19a8c010e8431bf44da7c36172924e727a
petar.andreev <petar.andreev@ysura.com>
2025-11-28 06:56:56 +0100

    glasses

---
+++ glasses/glasses.js
@@ -0,0 +1,108 @@
+const beerGlasses = [
+    // Spain
+    {
+        name: 'Alhambra </br> Copa 330ml',
+        brewery: 'Alhambra',
+        country: 'Spain',
+        image: 'glasses/img/Spain/Alhambra_330ml.png'
+    },
+    {
+        name: '1906 Pelirroja </br> Copa 330ml',
+        brewery: 'Estrella Galicia',
+        country: 'Spain',
+        image: 'glasses/img/Spain/1906_Pelirroja_330ml.png'
+    },
+    {
+        name: 'Estrella Galicia </br> Caña',
+        brewery: 'Estrella Galicia',
+        country: 'Spain',
+        image: 'glasses/img/Spain/Estrella_Galicia_330ml.png'
+    },
+    {
+        name: 'El Aguila </br> Caña',
+        brewery: 'El Aguila',
+        country: 'Spain',
+        image: 'glasses/img/Spain/El_Aguila_330ml.png'
+    },
+    {
+        name: 'Victoria Malaga </br> Caña',
+        brewery: 'Victoria Malaga',
+        country: 'Spain',
+        image: 'glasses/img/Spain/Victoria_Malaga_330ml.png'
+    },
+    {
+        name: 'Dorada Pilsen </br> Caña',
+        brewery: 'Dorada',
+        country: 'Spain',
+        image: 'glasses/img/Spain/Dorada_330ml.png'
+    },
+    // Germany
+    {
+        name: 'Paulaner Helles </br> Maßkrug 1l',
+        brewery: 'Paulaner',
+        country: 'Germany',
+        image: 'glasses/img/Germany/Paulaner_1l.png'
+    },
+    {
+        name: 'Paulaner Helles </br> Goblet 400ml',
+        brewery: 'Paulaner',
+        country: 'Germany',
+        image: 'glasses/img/Germany/Paulaner_400ml.png'
+    },
+    {
+        name: 'Paulaner Weißbier </br> Glas 300ml',
+        brewery: 'Paulaner',
+        country: 'Germany',
+        image: 'glasses/img/Germany/Paulaner_Weiss_300ml.png'
+    },
+    {
+        name: 'Franziskaner Weißbier </br> Glas 500ml',
+        brewery: 'Franziskaner',
+        country: 'Germany',
+        image: 'glasses/img/Germany/Franziskaner_500ml.png'
+    },
+    {
+        name: 'Hofbräuhaus </br> Maßkrug 1l',
+        brewery: 'Hofbräuhaus',
+        country: 'Germany',
+        image: 'glasses/img/Germany/HB_1l.png'
+    },
+    {
+        name: 'Camba Sensorik </br> Glas 330ml',
+        brewery: 'Camba',
+        country: 'Germany',
+        image: 'glasses/img/Germany/Camba_330ml.png'
+    },
+    {
+        name: 'Kaltenberg </br> Glas 500ml',
+        brewery: 'Kaltenberg',
+        country: 'Germany',
+        image: 'glasses/img/Germany/Kaltenberg_500ml.png'
+    },
+    // Bulgaria
+    {
+        name: 'Gulden Draak </bg> 500ml',
+        brewery: 'Gulden Draak',
+        country: 'Belgium',
+        image: 'glasses/img/Belgium/Gulden_Draak_500ml.png'
+    },
+    {
+        name: 'Brasserie d\'Achouffe </bg> 330ml',
+        brewery: 'Brasserie d\'Achouffe',
+        country: 'Belgium',
+        image: 'glasses/img/Belgium/Achouffe_330ml.png'
+    },
+    {
+        name: 'Abbaye d\'Aulne </bg> 330ml',
+        brewery: 'Abbaye d\'Aulne',
+        country: 'Belgium',
+        image: 'glasses/img/Belgium/Abbaye_dAulne_330ml.png'
+    },
+    // Bulgaria
+    {
+        name: 'Gaida Premium </br> 500ml',
+        brewery: 'Gaida',
+        country: 'Germany',
+        image: 'glasses/img/Bulgaria/Gaida_500ml.png'
+    },
+];
